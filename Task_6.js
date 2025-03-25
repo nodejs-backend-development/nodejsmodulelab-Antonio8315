e71d87a36d9f85a5033d4d292e1b83e754a6539f
Antonio8315 <berehii.anton@chnu.edu.ua>
2025-03-25 15:48:45 +0200

    attempt4

--- Task_6.js
+++ Task_6.js
@@ -3,7 +3,6 @@ const { Transform } = require('stream');
 class CustomStream extends Transform {
     _transform(chunk, encoding, callback) {
         const transformedChunk = chunk.toString().replace(/[a-z]/g, char => char.toUpperCase());
-        console.log(transformedChunk);
         this.push(transformedChunk);
         callback();
     }
